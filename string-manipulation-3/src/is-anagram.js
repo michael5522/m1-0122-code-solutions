4a91c47f569f6fc47e1fa03db7835178f8d5cc25
Michael Chang <michaelchang.kuo@gmail.com>
2022-03-10 15:31:35 -0800

    working on it

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
 /* exported isAnagram */
+
 function isAnagram(firstString, secondString) {
   var firstSplit = firstString.split(' ').join('').split('');
-  console.log(firstSplit);
+  // console.log(firstSplit);
   var secondSplit = secondString.split(' ').join('').split('');
-  console.log(secondSplit);
+  // console.log(secondSplit);
 
   var gg = secondSplit.length;
   var counter = 0;
@@ -15,12 +16,10 @@ function isAnagram(firstString, secondString) {
     }
   }
   var vital = newArray.length;
-  console.log('vital', newArray.length);
+  // console.log('vital', newArray.length);
   for (var i = 0; i < secondSplit.length; i++) {
     if (firstSplit.includes(secondSplit[i])) {
-      console.log('it this char is inside the first string---: ', secondSplit[i]);
-      // console.log('counter: ', counter);
-      // console.log('first array', firstSplit);
+      // console.log('it this char is inside the first string---: ', secondSplit[i]);
 
       for (var k = 0; k < vital; k++) {
         var pp = 0;
@@ -31,45 +30,26 @@ function isAnagram(firstString, secondString) {
             break;
           } else if (firstSplit.splice(k, 1)) {
             counter++;
-            console.log('current counter in loop', counter);
+            // console.log('current counter in loop', counter);
           }
         }
       }
-      console.log('12312321323213 need match', firstSplit[k], secondSplit[i]);
+      // console.log('12312321323213 need match', firstSplit[k], secondSplit[i]);
       if (firstSplit.splice(k, 1)) {
         counter++;
-        console.log('current counter in loop', counter);
+        // console.log('current counter in loop', counter);
       }
-      console.log('whats left of original array,', firstSplit);
+      // console.log('whats left of original array,', firstSplit);
 
     }
   }
 
   if (counter === gg) {
-    console.log('XXXXXXtotal length:', gg, counter);
-    console.log('AAAAAAtotal length:', gg, counter);
+    // console.log('XXXXXXtotal length:', gg, counter);
+    // console.log('AAAAAAtotal length:', gg, counter);
     return true;
   }
-  console.log('XXXXXXsfdsafdasffdsfdsaftotal length:', gg, counter);
-  console.log('XXXXXXtotal length:', gg, counter);
+  // console.log('XXXXXXsfdsafdasffdsfdsaftotal length:', gg, counter);
+  // console.log('XXXXXXtotal length:', gg, counter);
   return false;
 }
-
-// function isAnagram(firstString, secondString) {
-//   var firstSplit = firstString.split(' ').join('').split('');
-//   var secondSplit = secondString.split(' ').join('').split('');
-
-//   var secondSplitLength = secondSplit.length;
-//   var counter = 0;
-//   for (var i = 0; i < secondSplit.length; i++) {
-//     if (firstSplit.includes(secondSplit[i])) {
-//       counter++;
-//       continue;
-//     }
-//   }
-
-//   if (counter === secondSplitLength) {
-//     return true;
-//   }
-//   return false;
-// }

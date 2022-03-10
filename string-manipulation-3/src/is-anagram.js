87691055e7918371e8b1162d41d55c2c126adb99
Michael Chang <michaelchang.kuo@gmail.com>
2022-03-10 14:49:23 -0800

    working in progress

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+/* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var firstSplit = firstString.split(' ').join('').split('');
+  console.log(firstSplit);
+  var secondSplit = secondString.split(' ').join('').split('');
+  console.log(secondSplit);
+
+  var gg = secondSplit.length;
+  var counter = 0;
+  var newArray = [];
+  for (var a = 0; a < secondSplit.length; a++) {
+    if (firstSplit.includes(secondSplit[a])) {
+      newArray.push(secondSplit[a]);
+    }
+  }
+  var vital = newArray.length;
+  console.log('vital', newArray.length);
+  for (var i = 0; i < secondSplit.length; i++) {
+    if (firstSplit.includes(secondSplit[i])) {
+      console.log('it this char is inside the first string---: ', secondSplit[i]);
+      // console.log('counter: ', counter);
+      // console.log('first array', firstSplit);
+
+      for (var k = 0; k < vital; k++) {
+        var pp = 0;
+        if (firstSplit[k] === secondSplit[i]) {
+          // console.log('index', k);
+          pp++;
+          if (pp === 1) {
+            break;
+          } else if (firstSplit.splice(k, 1)) {
+            counter++;
+            console.log('current counter in loop', counter);
+          }
+        }
+      }
+      console.log('12312321323213 need match', firstSplit[k], secondSplit[i]);
+      if (firstSplit.splice(k, 1)) {
+        counter++;
+        console.log('current counter in loop', counter);
+      }
+      console.log('whats left of original array,', firstSplit);
+
+    }
+  }
+
+  if (counter === gg) {
+    console.log('XXXXXXtotal length:', gg, counter);
+    console.log('AAAAAAtotal length:', gg, counter);
+    return true;
+  }
+  console.log('XXXXXXsfdsafdasffdsfdsaftotal length:', gg, counter);
+  console.log('XXXXXXtotal length:', gg, counter);
+  return false;
+}
+
+// function isAnagram(firstString, secondString) {
+//   var firstSplit = firstString.split(' ').join('').split('');
+//   var secondSplit = secondString.split(' ').join('').split('');
+
+//   var secondSplitLength = secondSplit.length;
+//   var counter = 0;
+//   for (var i = 0; i < secondSplit.length; i++) {
+//     if (firstSplit.includes(secondSplit[i])) {
+//       counter++;
+//       continue;
+//     }
+//   }
+
+//   if (counter === secondSplitLength) {
+//     return true;
+//   }
+//   return false;
+// }
---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,23 @@
+/* exported isPalindromic */
+function isPalindromic(string) {
+  var reverse = [];
+  var array1 = string.split(' ').join('').split('');
+  // console.log('111 string split', array1);
+  // console.log('hi', array1.length);
+
+  for (var i = array1.length - 1; i >= 0; i--) {
+    reverse.push(array1[i]);
+  }
+  // console.log('222reverse of th', reverse);
+  var sen1 = array1.join('');
+  // console.log('111.111', sen1);
+  var sen2 = reverse.join('');
+  // console.log('222.222', sen2);
+
+  if (sen1 === sen2) {
+    // console.log('---same--');
+    return true;
+  }
+  // console.log('--fail--');
+  return false;
+}

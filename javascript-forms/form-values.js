ad55bbc3158962d35fbe2c6c3f59a5696240a1c6
Michael Chang <michaelchang.kuo@gmail.com>
2022-03-15 20:38:19 -0700

    added to callback function

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,7 +1,7 @@
 var contactForm = document.querySelector('#contact-form');
-var obj = {};
 contactForm.addEventListener('submit', function (event) {
   event.preventDefault();
+  var obj = {};
   obj.name = contactForm.elements.name.value;
   obj.email = contactForm.elements.email.value;
   obj.message = contactForm.elements.message.value;

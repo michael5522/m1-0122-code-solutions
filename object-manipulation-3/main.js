f451f12b7a6447fc1ad71dc6747714487f802183
Michael Chang <michaelchang.kuo@gmail.com>
2022-03-22 15:39:25 -0700

    finished object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -3,30 +3,20 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 var player1 = {
   hand: []
 };
-console.log(player1);
+// console.log(player1);
 
 var player2 = {
   hand: []
 };
-console.log(player2);
+// console.log(player2);
 var player3 = {
   hand: []
 };
-console.log(player3);
+// console.log(player3);
 var player4 = {
   hand: []
 };
-console.log(player4);
-
-// var cards = [];
-// for (suit = 4; suit > 0; suit--) {
-//   for (rank = 13; rank > 0; rank--) {
-//     cards.push({
-//       suit: suit,
-//       rank: rank
-//     });
-//   }
-// }
+// console.log(player4);
 
 const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
 const suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
@@ -38,7 +28,7 @@ for (var s = 0; s < suits.length; s++) {
     cards.push({ value, suit });
   }
 }
-console.log(cards);
+console.log('all 52 cards created', cards);
 
 var ContainerFor8Cards = [];
 function randomCards(cards) {
@@ -56,7 +46,7 @@ function randomCards(cards) {
 // console.log('beginning---');
 randomCards(cards);
 // console.log('distriubte card---s');
-// console.log('showing the current cards', ContainerFor8Cards);
+console.log('showing the randomly selected values of the 8 cards that will be distributed', ContainerFor8Cards);
 distributeCards(ContainerFor8Cards);
 function distributeCards(ContainerFor8Cards) {
   // console.log(ContainerFor8Cards.length);
@@ -65,34 +55,50 @@ function distributeCards(ContainerFor8Cards) {
     if (i < 2) {
       // console.log('triggering 1');
       player1.hand.push(ContainerFor8Cards[i]);
-      console.log('player1', player1);
+      // console.log('player1', player1);
     }
     if (i >= 2 && i < 4) {
       // console.log('triggering 2');
       player2.hand.push(ContainerFor8Cards[i]);
-      console.log('player2', player2);
+      // console.log('player2', player2);
     }
     if (i >= 4 && i < 6) {
       // console.log('triggering 3');
       player3.hand.push(ContainerFor8Cards[i]);
-      console.log('player3', player3);
+      // console.log('player3', player3);
     }
     if (i >= 6 && i < 8) {
       // console.log('triggering 4');
       player4.hand.push(ContainerFor8Cards[i]);
-      console.log('player4', player4);
+      // console.log('player4', player4);
     }
   }
 }
 
+var playerArray = [];
 function delcareWinner(player1, player2, player3, player4) {
   var player1Total = Number(player1.hand[0]) + Number(player1.hand[1]);
   var player2Total = Number(player2.hand[0]) + Number(player2.hand[1]);
   var player3Total = Number(player3.hand[0]) + Number(player3.hand[1]);
   var player4Total = Number(player4.hand[0]) + Number(player4.hand[1]);
-  console.log('player1Total:', player1Total);
-  console.log('player2Total:', player2Total);
-  console.log('player3Total:', player3Total);
-  console.log('player4Total:', player4Total);
+  console.log('player1 Total card value added:', player1Total);
+  console.log('player2 Total card value added:', player2Total);
+  console.log('player3 Total card value added:', player3Total);
+  console.log('player4 Total card value added:', player4Total);
+  playerArray.push(['player1', player1Total], ['player2', player2Total], ['player3', player3Total], ['player4', player4Total]);
+  console.log('Array with player number and total card value points: ', playerArray);
+
+  var winner = 0;
+  var winningPlayer = null;
+  for (var i = 0; i < 4; i++) {
+    // console.log('current location', playerArray[i]);
+    if (playerArray[i][1] > winner) {
+      // console.log('triggering');
+      winner = playerArray[i][1];
+      winningPlayer = playerArray[i];
+    }
+  }
+  // console.log('winner--', winner);
+  console.log('THE WINNER IS THIS PLAYER: ', `${winningPlayer[0]} with this amount of points total ${winner}`);
 }
 delcareWinner(player1, player2, player3, player4);
